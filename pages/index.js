5097f1d5734dc8df7d6b82cd1cab07209a54b19a
abyblackmouth <abraham.covarrubias@live.com>
2022-09-03 14:59:41 -0500

    login layaut is started

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,19 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
 <>
-<div className='bg-red-600'>Hola a todos</div>
+<nav className='bg-emerald-400'>
+      Bienvenido a Mi Fiesta.com
+    </nav>
+    <main className=''>
+      <section className='flex divide-red-400'>
+        <section className='h-2/5 w-1/2 bg-sky-400'>Aqui va la imagen introductiva</section>
+        <section className='h-2/5 w-1/2 bg-sky-200'>Aquí va el login
+        </section>
+      </section>
+    </main>
+    <><footer>Aqui va el foother</footer></>
+      
 </>
   )
 }
+    
