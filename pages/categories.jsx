e94c624395a0de0461171baffaa72947f8da7c30
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-09-13 20:04:53 -0500

    add categories

---
+++ pages/categories.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import Categories from "../components/Categories";
+import clsx from "clsx"
+
+export default function categories (){
+    return (
+    <>
+    <Categories/>
+    </>
+    )
+}
