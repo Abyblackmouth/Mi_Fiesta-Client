489fcd0527bb5132a4d9f8cc46420043ef2db37d
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-09-20 00:33:17 -0500

    layout form

---
+++ pages/provider-form.jsx
@@ -0,0 +1,14 @@
+import Button from "../components/Button"
+import FormCustomer from "../components/FormCustomer"
+import Layout from "../components/Layout"
+import H1 from "../components/Subtitle"
+
+export default function CustomerRegistration() {
+  return (
+
+    <Layout>
+    <FormCustomer> <H1>Hola mundo </H1> </FormCustomer>
+    </Layout>
+
+  )
+}
