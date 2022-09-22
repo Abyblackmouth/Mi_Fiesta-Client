83ef5274bba4e95921de7a6bdbc17dd09dacf4e9
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-09-22 18:14:37 -0500

    map in categories

--- pages/categories.jsx
+++ pages/categories.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import Steps from "../components/Steps";
+import Category from "../components/Category";
 
 export default function categories (){
+
+const [categories, setCategories] = useState([])
+
+useEffect( ()=>{
+    fetch("http://localhost:8081/category/")
+    .then((response)=> response.json())
+    .then(json =>{
+      setCategories(json.data)
+     
+    console.log(json.data)
+    })
+  },[])
+
     return (
     <>
     <Layout style='flex justify-center'>
         <Steps step='w-1/2'/>
+         
+         {
+            console.log("esto es categories",categories)
+          }
+        
     </Layout>
     </>
     )
