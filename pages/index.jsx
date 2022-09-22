9304ea6eb188489fdc1037f1f2634b5ab5f3925e
Noemi Gutierrez <noemigp0@gmail.com>
2022-09-21 19:21:08 -0500

    Discard commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '../components/Layout';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-
-    <Layout></Layout>
-  )
+    <>
+      <Navbar></Navbar>
+      {/* <Layout></Layout> */}
+    </>
+  );
 }
