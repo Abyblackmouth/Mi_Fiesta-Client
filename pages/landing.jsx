74a57be44522c741b2a9a2898a779bd370fc5e25
abyblackmouth <abraham.covarrubias@live.com>
2022-09-24 01:23:10 -0500

    footer, lists are added

--- pages/landing.jsx
+++ pages/landing.jsx
@@ -1,4 +1,5 @@
 import clsx from "clsx"
+import Button from "../components/Button"
 import SectionLayout from "../components/home/SectionLayout"
 
 export default function landing () {
@@ -14,10 +15,12 @@ export default function landing () {
         'justify-end ',
         )}>
           <img className=""></img>
-          <div className="flex justify-end items-center text-sm text-[#385890] font-bold ">
+          <div className="flex justify-end items-center text-sm text-[#385890] font-bold gap-6 mr-16">
             <a href="">Explora</a>
             <a href="">Registrate como proveedor</a>
             <a href="">Inicia sesion</a>
+            <Button etiqueta='COTIZA TU EVENTO' style='text-xs'></Button>
+            
           </div>
       </nav>
         <main className="bg-neutral-100">
@@ -93,27 +96,122 @@ export default function landing () {
                         <img className="image-service-card" src="/servicios/Exteriores.png" alt="Exteriores" />
                         </div> 
                 </section>
-            </SectionLayout>
-             <section className="bg-fuchsia-200">
-                Este es la seccion de musica
-            </section>
-            <section className="bg-sky-50">
-                Esta es la seccion de Comida
-            </section>
-            <section className="bg-yellow-500">
-                Esta es la seccion de Entretenimeitno
+
+             <section>
+                <h1 className="flex justify-start items-center text-slate-500 hover:text-blue-500 text-2xl my-8">
+                    Musica
+                </h1>
+                <div className="flex flex-row">
+                    <div className="w-1/2">
+                        <ol>
+                            <li className="text-slate-500 hover:text-blue-400">Grupo a capella para eventos</li>
+                            <li className="text-slate-500 hover:text-blue-400">Orquesta para eventos</li>
+                            <li className="text-slate-500 hover:text-blue-400">Banda musical</li>
+                            <li className="text-slate-500 hover:text-blue-400">Espectaculo de conjunto musical</li>
+                            <li className="text-slate-500 hover:text-blue-400">Concierto de quinteto</li>
+                            <li className="text-slate-500 hover:text-blue-400 mb-8">DJ</li>
+                        </ol>
+                    </div>
+                    <ol>
+                        <li className="text-slate-500 hover:text-blue-400">Maquinas de karaoke</li>
+                        <li className="text-slate-500 hover:text-blue-400">Dúo de musica</li>
+                        <li className="text-slate-500 hover:text-blue-400">Espectaculo de jazz</li>
+                        <li className="text-slate-500 hover:text-blue-400">Banda de mariachis</li>
+                        <li className="text-slate-500 hover:text-blue-400">Cuarteto de cuerdas para eventos</li>
+                        <li className="text-slate-500 hover:text-blue-400">Espectaculo de solista</li>
+                    </ol>
+                </div>
             </section>
             <section>
-                Aqui va unete a mi fiesta
+            <h1 className="flex justify-start items-center text-slate-500 hover:text-blue-500 text-2xl mb-8">
+                    Comida
+                </h1>
+                <div className="w-1/2 border-t-cyan-400"></div>
+                <div className="flex flex-row ">
+                    <div className="w-1/2">
+                        <ol>
+                            <li className="text-slate-500 hover:text-blue-400">Comida para un evento</li>
+                            <li className="text-slate-500 hover:text-blue-400">Catering movil</li>
+                            <li className="text-slate-500 hover:text-blue-400">Servicio de chef pesonal</li>
+                        </ol>
+                    </div>
+                    <div>
+                        <ol>
+                            <li className="text-slate-500 hover:text-blue-400">Sevico de bufett</li>
+                            <li className="text-slate-500 hover:text-blue-400">Pasteleria</li>
+                            <li className="text-slate-500 hover:text-blue-400">Servicio de mesero</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+            <section>
+            <h1 className="flex justify-start items-center text-slate-500 hover:text-blue-500 text-2xl my-8">
+                    Entretenimiento
+                </h1>
+                <div className="flex flex-row text-slate-500 hover:text-blue-400">
+                    <div  className="w-1/2">
+                        <ol>
+                            <li className="text-slate-500 hover:text-blue-400">Shows infantiles</li>
+                            <li className="text-slate-500 hover:text-blue-400">Payasos</li>
+                            <li className="text-slate-500 hover:text-blue-400">Animadores</li>
+                        </ol>
+                    </div>
+                    <div>
+                        <ol>
+                            <li className="text-slate-500 hover:text-blue-400">Magos</li>
+                            <li className="text-slate-500 hover:text-blue-400">Espectaculos con robots</li>
+                            <li className="text-slate-500 hover:text-blue-400">Stand Up </li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+            <h1 className="flex justify-center items-center h-36 text-slate-500 text-3xl">Unete a Mi Fiesta</h1>
+
+            <section className="flex justify-center w-full text-center">
+                <div className="max-w-sm rounded overflow-hidden shadow-lg w-1/3 m-2 text-slate-700">
+                    <img className="w-full" src="/cardsHome/Chef.jpg" alt="Chef"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Registra tu restaurante</div>
+                        <p class="text-gray-700 text-base">
+                        Accede a nuestros usuarios y disfruta de la logistica que ofrecemos.
+                        </p>
+                    </div>
+                    <div class="px-6 pt-1 pb-2">
+                        <Button etiqueta='SABER MAS'style='text-xs'></Button>
+                    </div>
+                </div>
+                
+                <div className="max-w-sm rounded overflow-hidden shadow-lg w-1/3 m-2 text-slate-700">
+                    <img className="w-full" src="/cardsHome/Negocio-1.jpg" alt="Persona en su negocio"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Registra tu negocio</div>
+                        <p class="text-gray-700 text-base">
+                        Descubre los beneficios de asociarte con MiFiesta para impulzar tus ventas.
+                        </p>
+                    </div>
+                    <div class="px-6 pt-1 pb-2">
+                        <Button etiqueta='SABER MAS'style='text-xs'></Button>    
+                    </div>
+                </div>
+
+                <div className="max-w-sm rounded overflow-hidden shadow-lg w-1/3 m-2 text-slate-700">
+                    <img className="w-full" src="/cardsHome/PersonaGlobos.jpeg" alt="Persona con globos"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Cotiza tu evento</div>
+                        <p class="text-gray-700 text-base">
+                            Busca los mejores profecionales y ahorra tiempo para crear tu evento.
+                        </p>
+                    </div>
+                    <div class="px-6 pt-1 pb-2">
+                    <Button etiqueta='SABER MAS'style='text-xs'></Button>
+                    </div>
+                </div>
             </section> 
+            </SectionLayout>
          </main> 
          <footer>
-            Aqui va el footer
+             <img className= "bg-hero w-full h-64 bg-cover"/>
         </footer> 
-
-        
-        
-        
         </>
     )
 }
