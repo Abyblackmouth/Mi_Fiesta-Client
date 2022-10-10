adf72546194f5142793172672fcc0e5932546469
Victor Reyes <jvreyesa@hotmail.com>
2022-10-09 19:20:10 -0500

    listado Productos

--- components/provider/CardCalendar.jsx
+++ components/provider/CardCalendar.jsx
@@ -1,42 +1,48 @@
+import React, { useState } from 'react'
 import clsx from "clsx"
 import Subtitle from "../Subtitle"
 import Button from "../Button"
 import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 export default function CardCalendar() {
+
+    const [value, onChange] = useState(new Date());
+
+
+   
+
   return (
     <>
 
-      
-    <div className="w-2/3 overflow-hidden flex ">
+        <div className="w-2/3 overflow-hidden flex ">
 
-        <article className={clsx( // 'w-60 px-8 ', 'mx-5',  'bg-white',
-            'w-30',
-            'gap-y-4',
-            'px-6 ',
-            'bg-gray-50',
-            'bg-opacity-50',
-            'shadow-md',
-            'rounded-md',
-            'flex flex-col items-center')}>
+            <article className={clsx( //'w-60 px-8 ', //'mx-5',  // 'bg-blue',
+                'gap-y-4',
+                'px-6',
+                'bg-gray-50',
+                'bg-opacity-50',
+                'shadow-md',
+                'rounded-lg',
+                'flex flex-col items-center')}>
 
-            <div className={clsx( 'my-5' )}>
+                <div className={clsx( 'my-5' )}>
 
-              <Subtitle>Calendario</Subtitle>
+                    <Subtitle>Calendario</Subtitle>
 
-            </div>
+                </div>
 
-            {/* Cards */}
+                {/* Cards */}
 
-            <div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex flex-col items-center p-0 max-w-xs bg-blue-500 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
 
-                <Calendar />
+                    <Calendar onChange={onChange} value={value} />
 
-            </div>
-        
-        </article>    
-        
-    </div>
+                </div> 
+            
+            </article>    
+            
+        </div>
 
     </>)
 }
